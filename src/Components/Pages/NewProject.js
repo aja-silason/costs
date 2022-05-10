cdb00f174ca7664e357c1781205892bb6bf34cc0
ananias augusto <ananiasjaimeaugusto@gmail.com>
2022-05-10 18:28:51 +0100

    Finalizando components do form, input, button e select, incluindo todos ao project e newproject

--- src/Components/Pages/NewProject.js
+++ src/Components/Pages/NewProject.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import ProjectForm from '../project/ProjectForm';
+
+import styles from './NewProject.module.css'
 
 export default function NewProject(){
     return(
-        <React.Fragment>
-            <div>
-                <h1>Novo Projecto</h1>
-            </div>
-        </React.Fragment>
-    );
+        <div className={styles.newproject_conteiner}>
+            <h1>Criar Projecto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm btnText="Criar Projeto"/>
+        </div>
+    )
 } 
