2878d78125c049061bb89a145277d90216a22447
ananias augusto <ananiasjaimeaugusto@gmail.com>
2022-05-10 17:18:35 +0100

    Adicionando o componente botão

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Home.module.css'
 
 import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton';
 
 export default function Home(){
     return(
@@ -9,9 +10,9 @@ export default function Home(){
             <h1>Bem-vindo ao <span>Costs</span></h1> 
             <p>Comece a gerênciar os seus projetos agora mesmo!</p>
 
-            <a href='#'>Criar Projeto</a>
+            <LinkButton to="newproject" text="Criar Projeto"/>
 
-            <img src={savings} alt="Savings"/>
+            <img src={savings} alt="Costs"/>
 
         </section>
     );
