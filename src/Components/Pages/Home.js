397125222f24a6ffe6f5e9fb644b0dbba575ef42
ananias augusto <ananiasjaimeaugusto@gmail.com>
2022-05-10 16:39:51 +0100

    Nem sei

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import styles from './Home.module.css'
+
+import savings from '../../img/savings.svg'
 
 export default function Home(){
     return(
-        <React.Fragment>
-            <div>
-                <h1>Home</h1>
-            </div>
-        </React.Fragment>
+        <section className={styles.home_conteiner}>
+            <h1>Bem-vindo ao <span>Costs</span></h1> 
+            <p>Comece a gerênciar os seus projetos agora mesmo!</p>
+
+            <a href='#'>Criar Projeto</a>
+
+            <img src={savings} alt="Savings"/>
+
+        </section>
     );
 } 
